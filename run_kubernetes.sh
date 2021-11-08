2eab42653d9fff7b309209ddc5ab0afa0cb9b713
chrisiregbu <ciregbu@gmail.com>
2021-11-08 20:51:51 +0000

    Updated virtual environment path

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ciregbu159/flaskapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskapp --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/flaskapp 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=ciregbu159/flaskapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 471776097205.dkr.ecr.eu-west-2.amazonaws.com
 
 # Step 3:
 # Push image to a docker repository
+docker login
+docker push ciregbu159/$dockerpath
