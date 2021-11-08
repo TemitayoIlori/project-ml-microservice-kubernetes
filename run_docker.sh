b1122382a2c2fdb58bc0dddbb8c0aa027b024d46
chrisiregbu <ciregbu@gmail.com>
2021-11-08 15:38:08 +0000

    Completing as per project requirements

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t aalatishe86/ml_app .
+docker build -t ciregbu159/microservice-app .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 ml_app
+docker run -p 8000:80 microservice-app
